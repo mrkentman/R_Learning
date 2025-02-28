34830de2e0a69bbd668243be98be7c0ddc6bbaeb
Jordan Kent <jordan@pcjjk.net>
2025-02-28 10:11:05 +0000

    Update and rename vigenere.R to Vigenere Cipher.R

--- Projects/2 - Cryptography/Vigenere Cipher.R
+++ Projects/2 - Cryptography/Vigenere Cipher.R
@@ -2,14 +2,13 @@
 #Load libaries
 library(dplyr)
 library(stringr)
-
 #----------------------------------
 #Functions used in both encoding and decoding
 
 #Function that lowers the message to lowercase and remove all punctuation
 filter_message <- function(text){
   filtered_text <- tolower(text) %>%
-  str_replace_all("[^[:alnum:]]","")
+    str_replace_all("[^[:alnum:]]","")
   return(filtered_text)
 }
 
@@ -25,9 +24,7 @@ get_char_positions <- function(char){
   as.numeric(charToRaw(char)) - as.numeric(charToRaw("a"))
 }
 
-
-#Encoding function
-vigenere_encode <- function(input_msg,keyword){
+vigenere_encode <- function(input_msg, keyword){
   
   #Filter both the message and keyword to make them usable
   msg_filtered <- filter_message(input_msg)
@@ -37,16 +34,25 @@ vigenere_encode <- function(input_msg,keyword){
   #Get the positions of the characters in the alphabet
   char_positions <- sapply(strsplit(msg_filtered,NULL)[[1]], get_char_positions)
   
+  #Make dataframe
   df <- data.frame(org_character = strsplit(msg_filtered,NULL)[[1]],org_position = char_positions)
-  #Asign each letter in the message a letter from the keyword and then gets the position of that letter in the alphabet
-  df$keyword_char <- rep(strsplit(keyword_filtered, NULL)[[1]], length.out = nrow(df))
-  df$keyword_char_pos <- apply(df["keyword_char"], 1, get_char_positions)
+  
+  #Create a vector of valid rows to use in future calculations
+  valid_rows <- df$org_position >=0 & df$org_position <= 25
+  
+  #Asign each letter in the message a letter from the keyword, repeating until no more message characters are left
+  df$keyword_char[valid_rows] <- rep(strsplit(keyword_filtered, NULL)[[1]], length.out = sum(valid_rows))
+  
+  #Get the position of the alphabet for each keyword character
+  df$keyword_char_pos[valid_rows] <- sapply(df$keyword_char[valid_rows], get_char_positions)
+  
   #Add the positions together and returns the position of where the coded character is in the alphabet
   #(We use modulo 26 because our alphabet is 26 characters long)
-  df$coded_char_pos <- (df$org_position + df$keyword_char_pos) %% 26
-  #If any numbers were in the original message, the orginal positions of them will be outside 0-25 so we make the 
-  #coded position the same as the original position so that they are preserved in the coded message
+  df$coded_char_pos[valid_rows] <- (df$org_position[valid_rows] + df$keyword_char_pos[valid_rows]) %% 26
+  
+  #Sets the coded_char_pos for any number rows back to the original position so it goes back to the original number in the input message
   df$coded_char_pos <- ifelse(df$org_position<0 | df$org_position>25, df$org_position, df$coded_char_pos)
+  
   #Convert the positions into the ASCII decimal equivalent and then converetd into the character
   df$coded_char_decimal <- df$coded_char_pos + 97
   df$coded_char <- sapply(df$coded_char_decimal, function(x) rawToChar(as.raw(x)))
@@ -54,10 +60,10 @@ vigenere_encode <- function(input_msg,keyword){
   #Pastes all the coded characters together to form the complete coded message
   coded_message <- paste0(df$coded_char, collapse="")
   
- return(coded_message) 
+  return(coded_message)
 }
 
-vigenere_decode <- function(input_msg,keyword){
+vigenere_decode <- function(input_msg, keyword){
   
   #Filter both the message and keyword to make them usable
   msg_filtered <- filter_message(input_msg)
@@ -67,15 +73,25 @@ vigenere_decode <- function(input_msg,keyword){
   #Get the positions of the coded characters in the alphabet
   char_positions <- sapply(strsplit(msg_filtered,NULL)[[1]], get_char_positions)
   
+  #Make dataframe
   df <- data.frame(coded_char = strsplit(msg_filtered,NULL)[[1]],coded_char_pos = char_positions)
-  #Asign each letter in the coded message a letter from the keyword and then gets the position of that letter in the alphabet
-  df$keyword_char <- rep(strsplit(keyword_filtered, NULL)[[1]], length.out = nrow(df))
-  df$keyword_char_pos <- apply(df["keyword_char"], 1, get_char_positions)
+  
+  #Create a vector of valid rows to use in future calculations
+  valid_rows <- df$coded_char_pos >=0 & df$coded_char_pos <= 25
+  
+  #Asign each letter in the message a letter from the keyword, repeating until no more message characters are left
+  df$keyword_char[valid_rows] <- rep(strsplit(keyword_filtered, NULL)[[1]], length.out = sum(valid_rows))
+  
+  #Get the position of the alphabet for each keyword character
+  df$keyword_char_pos[valid_rows] <- sapply(df$keyword_char[valid_rows], get_char_positions)
+  
   #Subtract the positions from each other and returns the position of where the original character is in the alphabet
   #(We use modulo 26 because our alphabet is 26 characters long)
-  df$org_char_pos <- (df$coded_char_pos - df$keyword_char_pos) %% 26
-  #Any characters in the coded text that fall outside 0-25 are numbers so we put the original character position equal to the coded one
+  df$org_char_pos[valid_rows] <- (df$coded_char_pos[valid_rows] - df$keyword_char_pos[valid_rows]) %% 26
+  
+  #Sets the org_char_pos for any number rows back to the coded position so it goes back to the correct number in the message
   df$org_char_pos <- ifelse(df$coded_char_pos < 0 | df$coded_char_pos > 25, df$coded_char_pos, df$org_char_pos)
+  
   #Converts the decimal into the character for the decoded message
   df$org_char_decimal <- df$org_char_pos + 97
   df$org_char <- sapply(df$org_char_decimal, function(x) rawToChar(as.raw(x)))
@@ -85,5 +101,3 @@ vigenere_decode <- function(input_msg,keyword){
   
   return(decoded_message)
 }
-
-#make it skip over numbers when assigning characters from the keyword
